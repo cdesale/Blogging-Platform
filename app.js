e38ab310504985729b0ed6dd84266d0053280ea5
Chaitali Desale <cdesal01@student.bbk.ac.uk>
2022-11-13 17:36:00 +0000

    Commented on users and added posts routes - Added comments on users - Added posts route - Added GET /posts/ to return all posts

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ require('dotenv/config')
 
 const app = express()
 const usersRoute = require('./routes/users')
+const postsRoute = require('./routes/posts')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 //2. Middleware.
 app.use(bodyParser.json())
 app.use('/users', usersRoute)
+app.use('/posts', postsRoute)
 mongoose.connect(process.env.DB_CONNECTOR, ()=>{
     console.log('DB is connected')
 })
--- validations/validation.js
+++ validations/validation.js
@@ -1,4 +1,5 @@
 const joi = require('joi')
+const jsonwebtoken = require('jsonwebtoken')
 
 const EMAIL_VALIDATION = joi.string().required().min(6).max(256).email()
 const PASSWORD_VALIDATION = joi.string().required().min(6).max(1024)
@@ -22,5 +23,19 @@ const loginFormValidation = (data)=>{
     return schemaValidation.validate(data)
 }
 
+const verifyToken = (req, res, next)=>{
+    const token = req.header('auth-token')
+    if(!token){
+        return res.status(401).send({message:'Access denied'})
+    }
+    try{
+        const verified = jsonwebtoken.verify(token, process.env.TOKEN_SECRET)
+        next()
+    } catch(err){
+        return res.status(401).send({message:'Invalid token'})
+    }
+}
+
 module.exports.registerFormValidation = registerFormValidation
 module.exports.loginFormValidation = loginFormValidation
+module.exports.verifyToken = verifyToken
